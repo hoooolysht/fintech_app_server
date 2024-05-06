7636edb448bb04bbc916132b181e91ccee69662b
999.kings <hoooolysht@gmail.com>
2024-05-07 01:15:36 +0300

    didn't change, little bit formatted

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan  from "morgan";
+import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
-import KPI from  "./models/KPI.js";
+import KPI from "./models/KPI.js";
 import { kpis } from "./data/data.js";
-
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -21,21 +20,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
 /* ROUTE SETUP */
 app.use("/kpi", kpiRoutes);
 
-
 /* MOGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL)
-.then(async () => {
-    app.listen(PORT,  () => console.log(`Server running on port: ${PORT}`))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
     /* APP DATA ONE TIME ONLY OR AS NEED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis).catch((err) => console.error(err)).finally(() => {
     //     console.log('Data added to database');
     //     });
-    })
-.catch((error) => console.log(`${error} did not connect`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
